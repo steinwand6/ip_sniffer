6f369d770eba0545e7f96259fc1b7133c4f41a14
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-08 02:03:02 +0900

    feat: implemented simple IP sniffer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{
+    env,
+    net::{IpAddr, TcpStream},
+};
+
+const MAX: u16 = 65535;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: ip_sniffer <ip>");
+        return;
+    }
+
+    let ip_addr: IpAddr = args[1]
+        .parse()
+        .expect("Unable to parse param as IP address");
+
+    for port in 0..MAX {
+        match TcpStream::connect((ip_addr, port)) {
+            Ok(_) => println!("{} is open!", port),
+            Err(_) => (),
+        }
+    }
 }
